e7768f5ec698af63bc88e65f7016302c77b952b8
Kobayashi <kobahide789@gmail.com>
2010-12-10 00:42:05 +0900

    SSL対応とかしてみた

--- examples/example.js
+++ examples/example.js
@@ -1,14 +1,14 @@
 var sys = require('sys');
 var pop3 = require('../lib/pop3');
 
-var auth = pop3.POPClient.authentication.apop("steve", "test");
+var auth = pop3.POPClient.authentication.apop("[gmail user]", "[gmail pass]");
 
-var client = new pop3.POPClient(110, "localhost.com", auth);
+var client = new pop3.POPClient(995, "pop.gmail.com", auth);
 client.addListener("authenticate", function(){
 	sys.puts("we have authenticated!");
 	client.getNumberOfMessages(function(num){
 		sys.puts("We have " + num + " messages");
-		client.getMessage(num-1, function(msg){
+		client.getMessage(num, function(msg){
 			sys.puts("-\nMessage!\n\n" + msg)
 			client.disconnect();
 		});
